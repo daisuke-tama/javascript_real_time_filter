640e45c114a9254be7aa32b1853f5d2c72c1b6f0
EC2 Default User <ec2-user@ip-172-31-12-161.us-east-2.compute.internal>
2021-03-29 06:47:03 +0000

    リアルタイムフィルター

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  
+  function Search(text) {
+    $(".keyword-list").each(function(index,element) {
+      let keyword = $(element).text();
+      if(keyword.indexOf(text) === -1) {
+        $(element).parent().css("display","none");
+      }
+      else {
+        $(element).parent().css("display","block");
+      }
+    })
+  }
+  
+  $(".input-text").on("input", function(event) {
+    let text = $(".input-text").val();
+    if(text === "") {
+      $(".filter-img").css("display","block");
+    }
+      else {
+        Search(text);
+      }
+  })
+})
